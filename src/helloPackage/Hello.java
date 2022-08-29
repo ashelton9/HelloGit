9f1e831a6e017e18daceef54591531c1873eebb5
avery <avery@Avery_Laptop>
2022-08-29 16:25:53 -0500

    Added my name and major

--- src/helloPackage/Hello.java
+++ src/helloPackage/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
  public static void main(String[] args) {
   System.out.println("Ariana Shelton");
   System.out.println("Computer Science");
+  System.out.println("Avery Monson");
+  System.out.println("Computer Engineering");
  }
  
 }
